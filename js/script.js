aa26523ea4de0fd916c78be6b9e89126fdf528c9
gabswyl <biel.vfo@gmail.com>
2023-11-06 21:57:28 -0300

    segundo commit

--- js/script.js
+++ js/script.js
@@ -69,3 +69,26 @@ footerFaq.forEach(footerEvento);
 if (window.SimpleAnime) {
     new SimpleAnime();
 }
+
+// lista de carrinho dropdown 
+
+
+const listaCompra = document.querySelector('.listaCompras')
+const fecharCompra = document.querySelector('.bar_button')
+
+listaCompra.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const barCompra = document.querySelector('.bar_card')
+    barCompra.classList.toggle('ativo')
+
+})
+
+fecharCompra.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const barCompra = document.querySelector('.bar_card')
+    barCompra.classList.toggle('ativo')
+})
+
+
